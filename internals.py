8d4686253170aee751573eba258e126392f575aa
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2022-11-13 18:42:07 +0100

    refactored variable names to a more readable form

--- internals.py
+++ internals.py
@@ -1,14 +1,14 @@
 class Car:
-    def __init__(self, maker, model, color, maxspeed, numofseats,):
+    def __init__(self, maker, model, color, max_speed, num_of_seats, ):
         self.maker = maker
         self.model = model
         self.color = color
-        self.maxspeed = maxspeed
-        self.numofseats = numofseats
+        self.max_speed = max_speed
+        self.num_of_seats = num_of_seats
 
     def __str__(self):
         return f"{self.maker} {self.model}"
 
     def getcardata(self):
         return f"This car is a(n) {self.model} made by {self.maker}, in the color of {self.color}" \
-               f". It has a max speed of {self.maxspeed} with {self.numofseats} amount of seats."
+               f". It has a max speed of {self.max_speed} with {self.num_of_seats} amount of seats."
--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import atexit
 
 master = Tk()
 
-carslist = [internals.Car("Toyota", "Corola", "black", 152, 4), internals.Car("Honda", "Civic", "white", 158, 5),
-            internals.Car("Subaru", "Outback", "navy", 182, 5), internals.Car("Nissan", "Altima", "red", 188, 4),
-            internals.Car("Ford", "Escape", "aqua", 250, 5), internals.Car("Honda", "Accord", "grey", 192, 4),
-            internals.Car("Subaru", "Crosstrek", "grey", 182, 5), internals.Car("Mazda", "CX-5", "white", 256, 5),
-            internals.Car("Hyundai", "Tucson", "black", 261, 5), internals.Car("Nissan", "Rogue", "white", 201, 5),
-            internals.Car("Tesla", "Model 3", "blue", 283, 4), internals.Car("Jeep", "Wrangler", "green", 285, 5)]
+cars_list = [internals.Car("Toyota", "Corola", "black", 152, 4), internals.Car("Honda", "Civic", "white", 158, 5),
+             internals.Car("Subaru", "Outback", "navy", 182, 5), internals.Car("Nissan", "Altima", "red", 188, 4),
+             internals.Car("Ford", "Escape", "aqua", 250, 5), internals.Car("Honda", "Accord", "grey", 192, 4),
+             internals.Car("Subaru", "Crosstrek", "grey", 182, 5), internals.Car("Mazda", "CX-5", "white", 256, 5),
+             internals.Car("Hyundai", "Tucson", "black", 261, 5), internals.Car("Nissan", "Rogue", "white", 201, 5),
+             internals.Car("Tesla", "Model 3", "blue", 283, 4), internals.Car("Jeep", "Wrangler", "green", 285, 5)]
 
 filters = ["Maker", "Model", "Color", "Horsepower", "Number of seats"]
 
 cars = {}
-for c in carslist:
+for c in cars_list:
     cars[c] = 0
 
 
@@ -29,15 +29,15 @@ if os.path.exists("sales.txt"):
         print(f.split(':')[1])
     file.close()
 
-currentcar = StringVar()
-currentfilter = StringVar()
+current_car = StringVar()
+current_filter = StringVar()
 
 
-def buycar():
-    msg_box = askyesno("Are you sure", f"Are you sure you want to buy this car: {currentcar.get()}")
-    if msg_box and currentcar.get() in cars:
-        cars[currentcar.get()] += 1
-        print(f"{currentcar.get()} {cars.get(currentcar.get())}")
+def buy_car():
+    msg_box = askyesno("Are you sure", f"Are you sure you want to buy this car: {current_car.get()}")
+    if msg_box and current_car.get() in cars:
+        cars[current_car.get()] += 1
+        print(f"{current_car.get()} {cars.get(current_car.get())}")
 
 
 def exit_handler():
@@ -46,22 +46,30 @@ def exit_handler():
         file.write('%s:%s\n' % (key, value))
 
 
-labelcar = Label(master, text="Select a car:")
-labelcar.grid(row=0, column=0)
+def selection_changed():
+    label_selected_car.config(text=current_car.get())
 
-comboboxcar = Combobox(master, values=carslist, textvariable=currentcar)
-comboboxcar.grid(row=0, column=1)
 
-labelfilter = Label(master, text="Filter cars by:")
-labelfilter.grid(row=1, column=0)
+label_car = Label(master, text="Select a car:")
+label_car.grid(row=0, column=0)
 
-comboboxfilter = Combobox(master, values=filters, textvariable=currentfilter)
-comboboxfilter.grid(row=1, column=1)
+combobox_car = Combobox(master, values=cars_list, textvariable=current_car)
+combobox_car.grid(row=0, column=1)
 
-buybutton = Button(master, text="Buy selected car", command=buycar)
-buybutton.grid(row=3, columnspan=2)
+label_filter = Label(master, text="Filter cars by:")
+label_filter.grid(row=1, column=0)
+
+combobox_filter = Combobox(master, values=filters, textvariable=current_filter)
+combobox_filter.grid(row=1, column=1)
+combobox_filter.bind("<<ComboboxSelected>>", selection_changed)
+
+label_selected_car = Label(master, text="The selected car is:")
+label_selected_car.grid(row=2, columnspan=2)
+
+buy_button = Button(master, text="Buy selected car", command=buy_car)
+buy_button.grid(row=3, columnspan=2)
 
 # ellenőrzésre használt függvény
-print(carslist[random.randint(0, carslist.__len__()-1)])
+print(cars_list[random.randint(0, cars_list.__len__() - 1)])
 mainloop()
 atexit.register(exit_handler)
